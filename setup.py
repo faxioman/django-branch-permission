04c984d3ccc5e55317d697ab0ca468b9487680d5
Fabio Masini <fabio@rapidosoft.it>
2013-05-29 17:26:15 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='django-branch-permission',
     version='0.1.0',
-    packages=[],
+    packages=['branch-permission'],
     license='LGPL',
     long_description=open('README.txt').read(),
     requires=['Django>=1.5']
