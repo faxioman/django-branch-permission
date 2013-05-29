31995f2baaef4d72f9da608cd0ca80e4aa615acd
Fabio Masini <fabio@rapidosoft.it>
2013-05-29 18:05:40 +0200

    Version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-branch-permission',
-    version='0.1.0',
+    version='0.1.1',
     packages=['branch_permission'],
     license='LGPL',
     long_description=open('README.txt').read(),
